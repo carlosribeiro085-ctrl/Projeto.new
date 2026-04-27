931816b35977a696f4e29191a4447472780e4c64
carlosribeiro085-ctrl <carlosribeiro085@gmail.com>
2026-04-27 17:30:27 -0400

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,6 @@ def home():
     return "Olá, mundo! Este é meu primeiro deploy no Azure."
 
 if __name__ == "__main__":
-    app.run()
+    # O Azure define uma variável de ambiente chamada 'PORT'
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
